d7c87c6efef89d11fc847a4039e6daadd141b673
gunee7 <gunee7@naver.com>
2018-01-07 14:24:18 +0900

    실습중

--- code/ConsoleApplication4/ConsoleApplication4/Debug/ex05-05.c
+++ code/ConsoleApplication4/ConsoleApplication4/Debug/ex05-05.c
@@ -13,12 +13,17 @@ int main(void)
 	float a4 = 0.0;
 	float a5 = 0.0;
 
-	/*printf("정수값을 입력하세요 : \n");
+	printf("정수값을 입력하세요 : \n");
+	scanf("%d", &data);
+	printf("입력값 : %d\n", data);
+	printf("출력값 : %d\n", data);
+
+	printf("ACII 값을 얻을 정수값을 입력하세요 : \n");
 	scanf("%d", &data);
 	printf("input : %d\n", data);
 	printf("output : %c\n", data);
 
-	printf("정수값을 입력하세요 : \n");
+	printf("ACII 값을 얻을 정수값을 입력하세요 : \n");
 	scanf("%d", &data);
 	printf("input : %d\n", data);
 	printf("output : %c\n", data);
@@ -32,18 +37,18 @@ int main(void)
 	printf("input year: %d\n", year);
 	printf("input month: %d\n", month);
 	printf("input date: %d\n", date);
-	printf("Your birthday is %d. %d. %d.\n", year, month, date); */
+	printf("Your birthday is %d. %d. %d.\n", year, month, date);
 
-	printf("숫자를 차례대로 입력하세요(예 1.2 34.5) : \n");
+	printf("숫자를 차례대로 입력하세요 : \n");
 	scanf("%f %f %f %f %f", &a1, &a2, &a3, &a4, &a5);
 	printf("input : %.1f %.3f %.5f %.1f %.4f\n", a1, a2, a3, a4, a5);
-	printf("%.3f\n", a1);
-	printf("%.3f\n", a2);
-	printf("%.3f\n", a3);
-	printf("%.3f\n", a4);
-	printf("%.3f\n", a5);
+	printf("%9.3f\n", a1);
+	printf("%9.3f\n", a2);
+	printf("%9.3f\n", a3);
+	printf("%9.3f\n", a4);
+	printf("%9.3f\n", a5);
 
-	getchar();
+	system("pause");
 
 	return 0;
 
